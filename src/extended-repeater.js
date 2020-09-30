06889973eb88a1ac131af3cfc2cdf0bffc5124a9
Igor Nazarov <grizley.tery@gmail.com>
2020-09-30 14:22:37 +0300

    feat: add solutions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  let temp = [];
+  if (options.separator == undefined)
+    options.separator = '+';
+  if (options.additionSeparator == undefined)
+    options.additionSeparator = '|';
+  if (options.repeatTimes == undefined)
+    options.repeatTimes = 0;
+  if (options.additionRepeatTimes == undefined)
+    options.additionRepeatTimes = 0;
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (options.additionRepeatTimes == 0 && options.addition != undefined) {
+    temp += str;
+    temp += options.addition;
+  }
+
+  for (let a = 0; a < options.repeatTimes; a += 1) {
+    temp += str;
+
+    for (let b = 0; b < options.additionRepeatTimes; b += 1) {
+      temp += options.addition;
+      if (b < options.additionRepeatTimes - 1) {
+        temp += options.additionSeparator;
+      }
+    }
+
+    if (a < options.repeatTimes - 1) {
+      temp += options.separator;
+    }
+  }
+  return temp;
 };
-  
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error('arr - should be array!');
+  }
+  if (arr.length === 0) {
+    return arr;
+  }
+
+  const changedArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--discard-next":
+        i += 1;
+        break;
+      case "--discard-prev":
+        if (arr[i - 2] !== "--discard-next") {
+          changedArray.pop();
+        }
+        break;
+      case "--double-next":
+        if (i + 1 < arr.length) {
+          changedArray.push(arr[i + 1]);
+        }
+        break;
+      case "--double-prev":
+        if (i - 1 >= 0 && arr[i - 2] !== "--discard-next") {
+          changedArray.push(arr[i - 1]);
+        }
+        break;
+      default:
+        changedArray.push(arr[i]);
+    }
+  }
+  return changedArray;
 };
